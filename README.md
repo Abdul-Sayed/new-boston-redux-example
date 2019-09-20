798dd27b77352837455cf7d52a38e706b804cd3e
Abdul-Sayed <awsayed@alaska.edu>
2019-09-20 14:21:13 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# new-boston-redux-example
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/new-boston-redux-example)
---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import UserList from './UserList';
+import UserDetail from './UserDetail'
+import './App.css';
+
+
+const App = () => {
+  return (
+    <div>
+      <UserList />
+      <UserDetail />
+    </div>
+  );
+}
+
+export default App;
+
+
